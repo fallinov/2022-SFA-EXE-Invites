21c4cd272fdf84a147449a11b711e9fa86050ac6
Steve Fallet <steve.fallet@gmail.com>
2022-11-08 08:25:04 +0100

    1st

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * @author      Steve Fallet <steve.fallet@divtec.ch>
+ * @version     1.0
+ * @since       2016-09-23
+ *
+ * http://usejsdoc.org/
+ * http://www.w3schools.com/js/js_conventions.asp
+ */
+
+//http://www.jslint.com/help.html
+/*jslint this:true */
+/*jslint es6, browser, devel, for, single, multivar*/
+/*global window, document, alert, performance */
+
+(function (document) {
+    "use strict";
+
+}(document)); //Main IIFE
