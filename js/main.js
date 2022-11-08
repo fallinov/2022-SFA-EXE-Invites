71a228a4317b47203c97e36552299b4ae358a699
Steve Fallet <steve.fallet@gmail.com>
2022-11-08 09:58:57 +0100

    Ajout donnée exercice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 /**
  * @author      Steve Fallet <steve.fallet@divtec.ch>
- * @version     1.0
+ * @version     4.0
  * @since       2016-09-23
- *
- * http://usejsdoc.org/
- * http://www.w3schools.com/js/js_conventions.asp
  */
