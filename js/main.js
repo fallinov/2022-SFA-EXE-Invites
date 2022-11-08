9ae29b521a484b79fae0b1fe4b241968a8c06d9d
Steve Fallet <steve.fallet@gmail.com>
2022-11-08 09:40:12 +0100

    nettoyage JS

--- js/main.js
+++ js/main.js
@@ -6,13 +6,3 @@
  * http://usejsdoc.org/
  * http://www.w3schools.com/js/js_conventions.asp
  */
-
-//http://www.jslint.com/help.html
-/*jslint this:true */
-/*jslint es6, browser, devel, for, single, multivar*/
-/*global window, document, alert, performance */
-
-(function (document) {
-    "use strict";
-
-}(document)); //Main IIFE
