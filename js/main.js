2d62d6d3282589d8b3feadea00515a38da78163c
Steve Fallet <steve.fallet@gmail.com>
2022-11-15 09:36:44 +0100

    demo sup

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@
  * @version     4.0
  * @since       2016-09-23
  */
+
+const ulInvites = document.querySelector('#invites');
+
+
+ulInvites.addEventListener('click', (e) => {
+    let cible = e.target;
+
+    if(cible.nodeName !== 'BUTTON') {
+        return;
+    }
+
+    cible.parentNode.remove();
+});
